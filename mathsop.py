d016c67416f58408e02e77168259b0d7a29ffe6d
Ruheena <ruheenashaik344@gmail.com>
2025-06-02 11:00:45 +0530

    mathematical operators

--- mathsop.py
+++ mathsop.py
@@ -1,3 +1,4 @@
+#operators
 a=input('Enter the first number: ')
 b=input('Enter the second number: ')
 a=int(a)
@@ -10,3 +11,11 @@ print("addition=",addition)
 print("subtraction=",subtraction)
 print("multiplication=",multiplication)
 print("division=",division)
+
+#string
+
+a=input('Enter your first name:')
+b=input('Enter your last name:')
+a=str(a)
+b=str(b)
+print("Hello,!"+a +" Welcome to the Python program.")
